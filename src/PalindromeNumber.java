d9e0716daf39eca554c09e5b835d63da22658b79
DESKTOP-5L5DCJB\Rick <zhaowwbb@gmail.com>
2026-04-15 08:48:33 -0400

    Palindrome Number V3, V4

--- src/PalindromeNumber.java
+++ src/PalindromeNumber.java
@@ -32,12 +32,40 @@ public class PalindromeNumber {
         return true;
     }
 
+    public boolean isPalindromeV3(int x) {
+        if(x < 0 || (x % 10 == 0 && x != 0))return false;
+        int rev = 0;
+        while(x > rev){
+            int digit = x % 10;
+            rev = rev * 10 + digit;
+            x /= 10;
+        }
+        if(rev == x || x == rev / 10)return true;
+        return false;
+    }
+
+    public boolean isPalindromeV4(int x) {
+        if(x < 0 || (x % 10 == 0 && x != 0))return false;
+        int rev = 0;
+        while(rev < x)
+        {
+            int digit = x % 10;
+            rev = rev * 10 + digit;
+            x /= 10;
+        }            
+        return (x == rev || x == rev / 10);
+    }
+
     public void test(int x, boolean expectedResult) {
+        System.out.println("###############################");
         boolean result = isPalindrome(x);
         System.out.printf("x=[%d],expected:[%b], actual:[%b]%n", x, expectedResult, result);
         result = isPalindromeV2(x);
         System.out.printf("x=[%d],expected:[%b], actual:[%b]%n", x, expectedResult, result);
-
+        result = isPalindromeV3(x);
+        System.out.printf("x=[%d],expected:[%b], actual:[%b]%n", x, expectedResult, result);
+        result = isPalindromeV4(x);
+        System.out.printf("x=[%d],expected:[%b], actual:[%b]%n", x, expectedResult, result);
     }
 
     public static void main(String[] args) {
