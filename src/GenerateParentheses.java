eb5915d9009ea56aab0977b8e05cd82f5337c185
DESKTOP-5L5DCJB\Rick <zhaowwbb@gmail.com>
2026-04-23 19:34:00 -0400

    Generate Parentheses V2

--- src/GenerateParentheses.java
+++ src/GenerateParentheses.java
@@ -30,11 +30,32 @@ public class GenerateParentheses {
         }
     }
 
+    public List<String> generateParenthesisV2(int n) {
+        List<String> result = new ArrayList<>();
+        backtraceV2(result, "", 0, 0, n);
+        return result;
+    }
+
+    public void backtraceV2(List<String> result, String str, int open, int close, int max) {
+        if (str.length() == 2 * max) {
+            result.add(str);
+            return;
+        }
+        if (open < max) {
+            backtraceV2(result, str + "(", open + 1, close, max);
+        }
+        if (close < open) {
+            backtraceV2(result, str + ")", open, close + 1, max);
+        }
+    }
+
     public void test(int n) {
         System.out.println("n=" + n);
         List<String> list = null;
         list = generateParenthesis(n);
-        System.out.println("list=" + list);
+        System.out.println("[V1]list=" + list);
+        list = generateParenthesisV2(n);
+        System.out.println("[V2]list=" + list);
     }
 
     public static void main(String[] args) {
