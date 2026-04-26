57a4d109ad04c841f03e908c2ec019b257e51a03
DESKTOP-5L5DCJB\Rick <zhaowwbb@gmail.com>
2026-04-26 11:29:05 -0400

    Longest Common Prefix V3 and V4

--- src/LongestCommonPrefix.java
+++ src/LongestCommonPrefix.java
@@ -48,12 +48,56 @@ public class LongestCommonPrefix {
         return sb.toString();
     }
 
+    public String longestCommonPrefixV3(String[] strs) {
+        String commonPrefix = "";
+        if (null == strs || strs.length == 0)
+            return commonPrefix;
+        Arrays.sort(strs);
+        int left = 0;
+        int right = strs.length - 1;
+        while (left < right) {
+            StringBuilder sb = new StringBuilder();
+            int index = 0;
+            String s1 = strs[left];
+            String s2 = strs[right];
+            while (index < s1.length() && index < s2.length() && s1.charAt(index) == s2.charAt(index)) {
+                sb.append(s1.charAt(index));
+                index++;
+            }
+            if (sb.length() > commonPrefix.length()) {
+                commonPrefix = sb.toString();
+            }
+            left++;
+            right--;
+        }
+
+        return commonPrefix;
+    }
+
+    public String longestCommonPrefixV4(String[] strs) {
+        if (null == strs || strs.length == 0)
+            return "";
+        String prefix = strs[0];
+        for (int i = 1; i < strs.length; i++) {
+            while (strs[i].indexOf(prefix) < 0) {
+                prefix = prefix.substring(0, prefix.length() - 1);
+            }
+            if (prefix.isEmpty())
+                return "";
+        }
+        return prefix;
+
+    }
+
     public void test(String[] strs, String expectedResult) {
         String result = longestCommonPrefix(strs);
-        System.out.printf("s=[%s], expected:[%s], actual:[%s] %n", Arrays.toString(strs), expectedResult, result);
+        System.out.printf("[V1]s=[%s], expected:[%s], actual:[%s] %n", Arrays.toString(strs), expectedResult, result);
         result = longestCommonPrefixV2(strs);
-        System.out.printf("s=[%s], expected:[%s], actual:[%s] %n", Arrays.toString(strs), expectedResult, result);
-
+        System.out.printf("[V2]s=[%s], expected:[%s], actual:[%s] %n", Arrays.toString(strs), expectedResult, result);
+        result = longestCommonPrefixV3(strs);
+        System.out.printf("[V3]s=[%s], expected:[%s], actual:[%s] %n", Arrays.toString(strs), expectedResult, result);
+        result = longestCommonPrefixV4(strs);
+        System.out.printf("[V4]s=[%s], expected:[%s], actual:[%s] %n", Arrays.toString(strs), expectedResult, result);
     }
 
     public static void main(String[] args) {
@@ -65,5 +109,14 @@ public class LongestCommonPrefix {
         strs = new String[] { "dog", "racecar", "car" };
         util.test(strs, "");
 
+        strs = new String[] {
+                "technique", // Length 9
+                "technician", // Length 10
+                "tech", // Length 4 (The Bottleneck)
+                "technology", // Length 10
+                "technical", // Length 9
+                "techie" // Length 6
+        };
+        util.test(strs, "tech");
     }
 }
