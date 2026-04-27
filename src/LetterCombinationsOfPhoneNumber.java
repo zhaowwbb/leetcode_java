a82cfba26549cabacfdd0093829b84d8e1c6d09a
DESKTOP-5L5DCJB\Rick <zhaowwbb@gmail.com>
2026-04-26 20:02:37 -0400

    Letter Combinations of a Phone Number V3, V4

--- src/LetterCombinationsOfPhoneNumber.java
+++ src/LetterCombinationsOfPhoneNumber.java
@@ -21,14 +21,15 @@ public class LetterCombinationsOfPhoneNumber {
 
     public List<String> letterCombinationsV2(String digits) {
         List<String> list = new ArrayList<>();
-        if(null == digits || digits.length() == 0)return list;
+        if (null == digits || digits.length() == 0)
+            return list;
 
         backtrace(list, new StringBuilder(), digits, 0);
         return list;
     }
 
-    public void backtrace(List<String> list, StringBuilder sb, String digits, int index){
-        if(digits.length() == index){
+    public void backtrace(List<String> list, StringBuilder sb, String digits, int index) {
+        if (digits.length() == index) {
             list.add(sb.toString());
             return;
         }
@@ -36,7 +37,7 @@ public class LetterCombinationsOfPhoneNumber {
 
         char[] charArray = letterMap[pos];
 
-        for(int i = 0; i < charArray.length; i++){
+        for (int i = 0; i < charArray.length; i++) {
             char c = charArray[i];
             sb.append(c);
             backtrace(list, sb, digits, index + 1);
@@ -45,7 +46,6 @@ public class LetterCombinationsOfPhoneNumber {
         }
 
         // backtrace(list, sb, digits, index + 1);
-
 
     }
 
@@ -57,15 +57,15 @@ public class LetterCombinationsOfPhoneNumber {
         for (int i = 0; i < digits.length(); i++) {
             char c = digits.charAt(i);
             int pos = c - '0';
-             System.out.println("pos=" + pos);
+            // System.out.println("pos=" + pos);
             // Integer key = Integer.valueOf(digits.charAt(i));
             char[] charArray = letterMap[pos];
-            System.out.println("charArray=" + Arrays.toString(charArray));
+            // System.out.println("charArray=" + Arrays.toString(charArray));
             addCombinations(list, charArray);
         }
         List<String> resultList = new ArrayList<String>();
-        for(String s : list){
-            if(s.length() == digits.length()){
+        for (String s : list) {
+            if (s.length() == digits.length()) {
                 resultList.add(s);
             }
         }
@@ -73,7 +73,7 @@ public class LetterCombinationsOfPhoneNumber {
     }
 
     public void addCombinations(List<String> list, char[] digitChars) {
-         System.out.println("addCombinations, list.size()=" + list.size());
+        // System.out.println("addCombinations, list.size()=" + list.size());
         List<String> tmpList = new ArrayList<>();
         for (int i = 0; i < digitChars.length; i++) {
             tmpList.add("" + digitChars[i]);
@@ -85,10 +85,8 @@ public class LetterCombinationsOfPhoneNumber {
         list.addAll(tmpList);
     }
 
-    public void test(String digits) {
-        System.out.println("digits=" + digits);
-        List<String> result = letterCombinations(digits);
-        System.out.println("result.size()=" + result.size());
+    private void printResult(List<String> result, String version) {
+        System.out.println("[" + version + "] result.size()=" + result.size());
         System.out.print("[");
         for (int i = 0; i < result.size(); i++) {
             System.out.print(result.get(i));
@@ -98,17 +96,63 @@ public class LetterCombinationsOfPhoneNumber {
         }
         System.out.print("]");
         System.out.println("");
-        
-        result = letterCombinationsV2(digits);
-        System.out.println("V2 result.size()=" + result.size());
-        System.out.print("[");
-        for (int i = 0; i < result.size(); i++) {
-            System.out.print(result.get(i));
-            if (i != result.size() - 1) {
-                System.out.print(",");
-            }
+    }
+
+    public List<String> letterCombinationsV3(String digits) {
+        List<String> result = new ArrayList<>();
+        StringBuilder sb = new StringBuilder();
+        backtraceV3(result, sb, digits, 0);
+        return result;
+    }
+
+    public void backtraceV3(List<String> result, StringBuilder sb, String digits, int index) {
+        if (sb.length() == digits.length()) {
+            result.add(sb.toString());
+            return;
         }
-        System.out.print("]");
+        int pos = digits.charAt(index) - '0';
+        char[] chars = letterMap[pos];
+        for (int i = 0; i < chars.length; i++) {
+            sb.append(chars[i]);
+            backtraceV3(result, sb, digits, index + 1);
+            sb.deleteCharAt(sb.length() - 1);
+        }
+    }
+
+    public List<String> letterCombinationsV4(String digits) {
+        List<String> result = new ArrayList<>();
+        backtrackV4(result, new StringBuilder(), digits, 0);
+        return result;
+    }
+
+    public void backtrackV4(List<String> result, StringBuilder sb, String digits, int index) {
+        if (sb.length() == digits.length()) {
+            result.add(sb.toString());
+            return;
+        }
+        int pos = digits.charAt(index) - '0';
+        char[] letters = letterMap[pos];
+        for (int i = 0; i < letters.length; i++) {
+            sb.append(letters[i]);
+            backtrackV4(result, sb, digits, index + 1);
+            sb.deleteCharAt(sb.length() - 1);
+        }
+    }
+
+    public void test(String digits) {
+        System.out.println("digits=" + digits);
+        List<String> result = letterCombinations(digits);
+        printResult(result, "V1");
+
+        result = letterCombinationsV2(digits);
+        printResult(result, "V2");
+
+        result = letterCombinationsV3(digits);
+        printResult(result, "V3");
+
+        result = letterCombinationsV4(digits);
+        printResult(result, "V4");
+        System.out.println("#########################");
     }
 
     public static void main(String[] args) {
