a039aacf8876b804b2faf9cecc878e0ed24d669a
DESKTOP-5L5DCJB\Rick <zhaowwbb@gmail.com>
2026-04-30 17:30:25 -0400

    Merge k Sorted Lists V3

--- src/MergeKSortedLists.java
+++ src/MergeKSortedLists.java
@@ -1,28 +1,27 @@
 import java.util.*;
 
 public class MergeKSortedLists {
-   
+
     public ListNode mergeKListsV1(ListNode[] lists) {
         ListNode dummyNode = new ListNode(0);
         ListNode current = dummyNode;
 
-        
-        while(true){
+        while (true) {
             int moveIndex = -1;
             int minVal = Integer.MAX_VALUE;
-            for(int i = 0; i < lists.length; i++){
+            for (int i = 0; i < lists.length; i++) {
                 // System.out.println("lists[i].val=" + lists[i].val + ", minVal=" + minVal);
-                if(lists[i] != null && lists[i].val < minVal){
+                if (lists[i] != null && lists[i].val < minVal) {
                     moveIndex = i;
                     minVal = lists[i].val;
                     // System.out.println("moveIndex=" + moveIndex + ",minVal=" + minVal);
                 }
             }
-            if(moveIndex < 0){
-                System.out.println("==== Done =====");
-                //done, 
+            if (moveIndex < 0) {
+                // System.out.println("==== Done =====");
+                // done,
                 break;
-            }else{
+            } else {
                 current.next = lists[moveIndex];
                 current = current.next;
                 lists[moveIndex] = lists[moveIndex].next;
@@ -30,37 +29,56 @@ public class MergeKSortedLists {
             }
         }
         return dummyNode.next;
-    }    
-
-public ListNode mergeKListsV2(ListNode[] lists) {
-    ListNode dummyNode = new ListNode(0);
-    ListNode current = dummyNode;
-    while(true){
-        int minVal = Integer.MAX_VALUE;
-        int moveIndex = -1;
-        for(int i = 0; i < lists.length; i++){
-            if(lists[i] != null && lists[i].val < minVal){
-                moveIndex = i;
-                minVal = lists[i].val;
-            } 
-        }
-        if(moveIndex < 0){
-            System.out.println("=====  Done =====");
-            break;
-        }else{
-            current.next = lists[moveIndex];
-            lists[moveIndex] = lists[moveIndex].next;
-            current = current.next;
-        }
     }
 
-    return dummyNode.next;
-}
+    public ListNode mergeKListsV2(ListNode[] lists) {
+        ListNode dummyNode = new ListNode(0);
+        ListNode current = dummyNode;
+        while (true) {
+            int minVal = Integer.MAX_VALUE;
+            int moveIndex = -1;
+            for (int i = 0; i < lists.length; i++) {
+                if (lists[i] != null && lists[i].val < minVal) {
+                    moveIndex = i;
+                    minVal = lists[i].val;
+                }
+            }
+            if (moveIndex < 0) {
+                // System.out.println("===== Done =====");
+                break;
+            } else {
+                current.next = lists[moveIndex];
+                lists[moveIndex] = lists[moveIndex].next;
+                current = current.next;
+            }
+        }
 
-    public void test(){
+        return dummyNode.next;
+    }
+
+    public ListNode mergeKListsV3(ListNode[] lists) {
+        ListNode dummy = new ListNode(0);
+        ListNode current = dummy;
+        PriorityQueue<ListNode> minHeap = new PriorityQueue<>((a, b) -> (a.val - b.val));
+        for (ListNode n : lists) {
+            minHeap.add(n);
+        }
+        while (!minHeap.isEmpty()) {
+            ListNode node = minHeap.poll();
+            current.next = node;
+            current = current.next;
+
+            if (node.next != null) {
+                minHeap.add(node.next);
+            }
+        }
+        return dummy.next;
+    }
+
+    public void test() {
         ListNode[] lists = createTestData();
         System.out.println("Input:");
-        for(ListNode n : lists){
+        for (ListNode n : lists) {
             Util.printNode(n);
         }
         ListNode result = mergeKListsV1(lists);
@@ -71,21 +89,27 @@ public ListNode mergeKListsV2(ListNode[] lists) {
         result = mergeKListsV2(lists);
         System.out.println("[V2] Merged result:");
         Util.printNode(result);
+
+        lists = createTestData();
+        result = mergeKListsV3(lists);
+        System.out.println("[V3] Merged result:");
+        Util.printNode(result);
     }
 
-    public static ListNode[] createTestData(){
-         int[] nums1 = { 1,4,5 };
-        int[] nums2 = { 1,3,4 };
-        int[] nums3 = { 2,6 };
+    public static ListNode[] createTestData() {
+        int[] nums1 = { 1, 4, 5 };
+        int[] nums2 = { 1, 3, 4 };
+        int[] nums3 = { 2, 6 };
         ListNode list1 = Util.createListNodeFromArray(nums1);
-        ListNode list2 = Util.createListNodeFromArray(nums2); 
-        ListNode list3 = Util.createListNodeFromArray(nums3);   
-        ListNode[] lists = new ListNode[] {list1, list2, list3};  
-        return   lists;         
-    } 
+        ListNode list2 = Util.createListNodeFromArray(nums2);
+        ListNode list3 = Util.createListNodeFromArray(nums3);
+        ListNode[] lists = new ListNode[] { list1, list2, list3 };
+        return lists;
+    }
+
     public static void main(String[] args) {
         MergeKSortedLists util = new MergeKSortedLists();
-     
+
         util.test();
     }
 }
