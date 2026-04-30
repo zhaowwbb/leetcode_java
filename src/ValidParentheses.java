ee44d2a803c8b7d43dba3a02b73d8c4ddb814f66
DESKTOP-5L5DCJB\Rick <zhaowwbb@gmail.com>
2026-04-30 10:07:52 -0400

    Valid Parentheses V3, V4

--- src/ValidParentheses.java
+++ src/ValidParentheses.java
@@ -1,5 +1,5 @@
 
-import java.util.Stack;
+import java.util.*;
 
 public class ValidParentheses {
 
@@ -59,12 +59,73 @@ public class ValidParentheses {
         return stack.isEmpty();
     }
 
+    public boolean isValidV3(String s) {
+        Stack<Character> stack = new Stack();
+        for (char c : s.toCharArray()) {
+            if (c == '(' || c == '{' || c == '[') {
+                stack.push(c);
+            } else {
+                if (c == ')') {
+                    if (!stack.empty() && stack.peek() == '(') {
+                        stack.pop();
+                    } else {
+                        return false;
+                    }
+                } else if (c == '}') {
+                    if (!stack.empty() && stack.peek() == '{') {
+                        stack.pop();
+                    } else {
+                        return false;
+                    }
+                } else if (c == ']') {
+                    if (!stack.empty() && stack.peek() == '[') {
+                        stack.pop();
+                    } else {
+                        return false;
+                    }
+                } else {
+                    return false;
+                }
+            }
+        }
+        return stack.isEmpty();
+    }
+
+    public boolean isValidV4(String s) {
+        if(null == s || s.length() % 2 != 0)return false;
+        Stack<Character> stack = new Stack<>();
+        Map<Character, Character> map = new HashMap<>();
+        map.put(')', '(');
+        map.put(']', '[');
+        map.put('}','{'); 
+        for(char c : s.toCharArray()){
+            if(map.containsKey(c)){
+                if(!stack.isEmpty()){
+                    char top = stack.pop();
+                    if(top != map.get(c)){
+                        return false;
+                    }
+                }else{
+                    return false;
+                }
+            }else{
+                stack.push(c);
+            }
+        }
+
+        return stack.isEmpty();
+    }
+
     public void test(String s, boolean expectedResult) {
         boolean result = isValid(s);
-        System.out.printf("s=[%s], expected:[%b], actual:[%b] %n", s, expectedResult, result);
+        System.out.printf("[V1] s=[%s], expected:[%b], actual:[%b] %n", s, expectedResult, result);
         result = isValidV2(s);
-        System.out.printf("s=[%s], expected:[%b], actual:[%b] %n", s, expectedResult, result);
-
+        System.out.printf("[V2] s=[%s], expected:[%b], actual:[%b] %n", s, expectedResult, result);
+        result = isValidV3(s);
+        System.out.printf("[V3] s=[%s], expected:[%b], actual:[%b] %n", s, expectedResult, result);
+        result = isValidV4(s);
+        System.out.printf("[V4] s=[%s], expected:[%b], actual:[%b] %n", s, expectedResult, result);
+        System.out.println("--------------------------------------------------");
     }
 
     public static void main(String[] args) {
