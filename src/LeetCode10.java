c299e0a55230d74d7ee31da929c69925586cc78c
DESKTOP-5L5DCJB\Rick <zhaowwbb@gmail.com>
2026-06-15 09:38:16 -0400

    LeetCode10 V9

--- src/LeetCode10.java
+++ src/LeetCode10.java
@@ -245,32 +245,60 @@ public class LeetCode10 {
     public boolean isMatchV8(String s, String p) {
         int m = s.length();
         int n = p.length();
-        boolean[][] dp = new boolean[m + 1][n+1];
-        dp[0][0]= true;
-        for(int j = 2; j <= n; j++){
-            if(p.charAt(j - 1) == '*'){
+        boolean[][] dp = new boolean[m + 1][n + 1];
+        dp[0][0] = true;
+        for (int j = 2; j <= n; j++) {
+            if (p.charAt(j - 1) == '*') {
                 dp[0][j] = dp[0][j - 2];
             }
         }
-        for(int i = 1; i <= m; i++){
-            for(int j = 1; j <= n; j++){
-                char currentC = s.charAt(i-1);
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                char currentC = s.charAt(i - 1);
                 char currentP = p.charAt(j - 1);
-                if(currentC == currentP || currentP == '.'){
-                    dp[i][j] = dp[i-1][j-1];
-                }else if(currentP == '*'){
-                    //0 occurrence
-                    dp[i][j] = dp[i][j-2];
+                if (currentC == currentP || currentP == '.') {
+                    dp[i][j] = dp[i - 1][j - 1];
+                } else if (currentP == '*') {
+                    // 0 occurrence
+                    dp[i][j] = dp[i][j - 2];
 
                     char preP = p.charAt(j - 2);
-                    if(preP == currentC || preP == '.'){
-                        dp[i][j] = dp[i][j] || dp[i-1][j];
+                    if (preP == currentC || preP == '.') {
+                        dp[i][j] = dp[i][j] || dp[i - 1][j];
                     }
                 }
             }
         }
 
         return dp[m][n];
+    }
+
+    public boolean isMatchV9(String s, String p) {
+        int sLen = s.length();
+        int pLen = p.length();
+        boolean[][] dp = new boolean[sLen + 1][pLen + 1];
+        dp[0][0] = true;
+        for (int j = 2; j <= pLen; j++) {
+            if (p.charAt(j-1) == '*') {
+                dp[0][j] = dp[0][j - 2];
+            }
+        }
+        for (int i = 1; i <= sLen; i++) {
+            for (int j = 1; j <= pLen; j++) {
+                char curC = s.charAt(i - 1);
+                char curP = p.charAt(j - 1);
+                if (curP == '.' || curC == curP) {
+                    dp[i][j] = dp[i - 1][j - 1];
+                } else if (curP == '*') {
+                    dp[i][j] = dp[i][j - 2];
+                    char preP = p.charAt(j - 2);
+                    if (preP == curC || preP == '.') {
+                        dp[i][j] = dp[i][j] || dp[i - 1][j];
+                    }
+                }
+            }
+        }
+        return dp[sLen][pLen];
     }
 
     public void test(String s, String p, boolean expectedResult) {
@@ -291,6 +319,8 @@ public class LeetCode10 {
         System.out.printf("V7 s=[%s], p=[%s], expected:[%b], actual:[%b]%n", s, p, expectedResult, result);
         result = isMatchV8(s, p);
         System.out.printf("V8 s=[%s], p=[%s], expected:[%b], actual:[%b]%n", s, p, expectedResult, result);
+        result = isMatchV9(s, p);
+        System.out.printf("V9 s=[%s], p=[%s], expected:[%b], actual:[%b]%n", s, p, expectedResult, result);
         System.out.println("##########################");
     }
 
