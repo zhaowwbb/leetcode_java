ad3a58a8fd612f7f896bbc2abbdb4a15f4577815
DESKTOP-5L5DCJB\Rick <zhaowwbb@gmail.com>
2026-06-17 11:19:36 -0400

    LeetCode16 V6, V7

--- src/LeetCode16.java
+++ src/LeetCode16.java
@@ -1,6 +1,6 @@
 import java.util.Arrays;
 
-public class ThreeSumClosest {
+public class LeetCode16 {
 
     public int threeSumClosestV2(int[] nums, int target) {
         if (null == nums || nums.length < 3)
@@ -124,27 +124,87 @@ public class ThreeSumClosest {
     }
 
     public int threeSumClosestV5(int[] nums, int target) {
-        if(null == nums || nums.length < 3)return 0;
+        if (null == nums || nums.length < 3)
+            return 0;
         int n = nums.length;
         int closestSum = nums[0] + nums[1] + nums[2];
 
-        for(int i = 0; i < n - 2; i++){
+        for (int i = 0; i < n - 2; i++) {
             int left = i + 1;
             int right = n - 1;
-            while(left < right){
+            while (left < right) {
                 int currentSum = nums[i] + nums[left] + nums[right];
-                if(currentSum == target)return currentSum;
-                if(Math.abs(target - currentSum) < Math.abs(target - closestSum)){
+                if (currentSum == target)
+                    return currentSum;
+                if (Math.abs(target - currentSum) < Math.abs(target - closestSum)) {
                     closestSum = currentSum;
-                }   
-                if(currentSum < target){
+                }
+                if (currentSum < target) {
                     left++;
-                }else{
+                } else {
                     right--;
                 }
             }
         }
         return closestSum;
+    }
+
+    public int threeSumClosestV6(int[] nums, int target) {
+        int closeSum = Integer.MAX_VALUE;
+        int preDistance = Integer.MAX_VALUE;
+        Arrays.sort(nums);
+        for (int i = 0; i < nums.length - 2; i++) {
+            int left = i + 1;
+            int right = nums.length - 1;
+            while (left < right) {
+                int sum = nums[i] + nums[left] + nums[right];
+                int distance = 0;
+                if ((sum > 0 && target > 0) || (sum < 0 && target < 0)) {
+                    distance = Math.abs(sum - target);
+                } else {
+                    distance = Math.abs(sum) + Math.abs(target);
+                }
+                if (distance < preDistance) {
+                    closeSum = sum;
+                    preDistance = distance;
+                }
+                // closeSum = Math.min(closeSum, distance);
+
+                if (sum == target) {
+                    return target;
+                } else if (sum > target) {
+                    right--;
+                } else {
+                    left++;
+                }
+            }
+        }
+        return closeSum;
+    }
+
+    public int threeSumClosestV7(int[] nums, int target) {
+        int closeSum = nums[0] + nums[1] + nums[2];
+        int len = nums.length;
+        for (int i = 0; i < len - 2; i++) {
+            // while (i > 0 && i < len - 2 && nums[i] == nums[i - 1])
+            //     continue;
+            int left = i + 1;
+            int right = len - 1;
+            while (left < right) {
+                int sum = nums[i] + nums[left] + nums[right];
+                if (Math.abs(sum - target) < Math.abs(closeSum - target)) {
+                    closeSum = sum;
+                }
+                if (sum == target) {
+                    return sum;
+                } else if (sum > target) {
+                    right--;
+                } else {
+                    left++;
+                }
+            }
+        }
+        return closeSum;
     }
 
     public void test(int[] nums, int target, int expectedResult) {
@@ -167,15 +227,26 @@ public class ThreeSumClosest {
         result = threeSumClosestV5(nums, target);
         System.out.printf("[V5], nums=[%s], expected:[%d], actual:[%d] %n", Arrays.toString(nums), expectedResult,
                 result);
+
+        result = threeSumClosestV6(nums, target);
+        System.out.printf("[V6], nums=[%s], expected:[%d], actual:[%d] %n", Arrays.toString(nums), expectedResult,
+                result);
+
+        result = threeSumClosestV7(nums, target);
+        System.out.printf("[V7], nums=[%s], expected:[%d], actual:[%d] %n", Arrays.toString(nums), expectedResult,
+                result);
         System.out.println("############################");
 
     }
 
     public static void main(String[] args) {
-        ThreeSumClosest util = new ThreeSumClosest();
+        LeetCode16 util = new LeetCode16();
         int[] nums = { -1, 2, 1, -4 };
         util.test(nums, 1, 2);
         nums = new int[] { 0, 0, 0 };
         util.test(nums, 1, 0);
+        nums = new int[] { 1,1,1,1 };
+        util.test(nums, 1, 3);
+
     }
 }
